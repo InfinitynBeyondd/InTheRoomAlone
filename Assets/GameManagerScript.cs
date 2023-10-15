0ba09b24a83c8fdbaf6f38a3854e8bae0b200cd3
Infinity <95947686+InfinitynBeyondd@users.noreply.github.com>
2023-10-15 09:03:41 -0400

    Added a countdown timer to the game

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -1,15 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.UI;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManagerScript : MonoBehaviour
 {
     public static GameManagerScript instance;
 
+    [Header("Reference to game objects")]
+    public GameObject Door1;
+    public DoorLogicScript doorLogic1;
+    public GameObject Door2;
+    public DoorLogicScript doorLogic2;
+    public GameObject Door3;
+    public DoorLogicScript doorLogic3;
+
+    public Text TimerCountdownText;
+
+    [Header("Important Numbers")]
+    [SerializeField] float remainingtime;
+    public int monsterRate = 0; //MonsterRate Determines how fast the monster bar fills up, it depends on how many doors are open and how many have been closed
+
+
+
+
     private void Awake()
     {
         instance = this;
+
     }
+
+    private void FixedUpdate()
+    {
+        remainingtime -= Time.deltaTime;
+        int minutes = Mathf.FloorToInt(remainingtime / 60);
+        int seconds = Mathf.FloorToInt(remainingtime % 60);
+        TimerCountdownText.text = string.Format("{0:00}:{1:00}",minutes,seconds);
+    }
+
+
 
     // Update is called once per frame
     void Update()
